06a3c20505d563a4613418d8916c5a604a4a2dca
Isaac Kim <kisaac95@yahoo.com>
2023-01-23 14:51:20 -0800

    added models

---
+++ migrations/20230123224917-create-event-stage.js
@@ -0,0 +1,17 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('event_stages', {
+      event_stage_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('event_stages');
+  }
+};
